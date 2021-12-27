b7d78d612bb4b1ed16a2b61bf91b559d657a5f97
ikadgzl <ikadgzl@gmail.com>
2021-12-27 07:42:33 +0300

    added delete feature for projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,23 +1,48 @@
+import { useNavigate } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
 
 const ProjectSummary = ({
-  project: { name, dueDate, details, assignedUsersList }
+  project: { id, name, dueDate, details, assignedUsersList, createdBy }
 }) => {
+  const navigate = useNavigate();
+  const { deleteDocument, response } = useFirestore('projects');
+  const { user } = useAuthContext();
+
+  const deleteProjectHandler = (e) => {
+    deleteDocument(id);
+
+    navigate('/');
+  };
+
   return (
-    <div className='project-summary'>
-      <h2 className='page-title'>{name}</h2>
-      <p className='due-date'>
-        Project due by {dueDate.toDate().toDateString()}{' '}
-      </p>
-      <p className='details'>{details}</p>
-      <h4>Project is assigned to:</h4>
-      <div className='assigned-users'>
-        {assignedUsersList.map(({ id, photoURL }) => (
-          <div key={id}>
-            <Avatar src={photoURL} />
-          </div>
-        ))}
+    <div>
+      <div className='project-summary'>
+        <h2 className='page-title'>{name}</h2>
+        <p>By {createdBy.displayName}</p>
+        <p className='due-date'>
+          Project due by {dueDate.toDate().toDateString()}{' '}
+        </p>
+        <p className='details'>{details}</p>
+        <h4>Project is assigned to:</h4>
+        <div className='assigned-users'>
+          {assignedUsersList.map(({ id, photoURL }) => (
+            <div key={id}>
+              <Avatar src={photoURL} />
+            </div>
+          ))}
+        </div>
       </div>
+      {user.uid === createdBy.id && (
+        <button
+          className='btn'
+          onClick={deleteProjectHandler}
+          disabled={response.isPending && true}
+        >
+          {!response.isPending ? 'Mark as Complete' : 'Marking...'}
+        </button>
+      )}
     </div>
   );
 };
